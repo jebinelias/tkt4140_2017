f0e1414eac5950e1b8d63bc833fd25fcc79c7aae
Fredf10 <fredrikeikelandfossan@dhcp-10-24-13-187.wlan.ntnu.no>
2017-01-12 12:24:14 +0100

    added test.py

---
+++ pythonExercises/Test.py
@@ -0,0 +1,12 @@
+##import numpy as np
+
+unsortedlist =[10, 9, 8, 7, 6]
+
+indice_low_to_high = sorted(range(len(unsortedlist)), key=lambda k: unsortedlist[k])
+
+print unsortedlist, indice_low_to_high
+
+
+
+
+    
